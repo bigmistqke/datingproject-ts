4765b4934fa330d3a4290e357d89d361fc65303d
Vincent Van Dijck <vandijckv@gmail.com>
2022-05-16 17:14:36 +0200

    editor

--- editor/client_solid/src/routes/List.jsx
+++ editor/client_solid/src/routes/List.jsx
@@ -1,44 +1,43 @@
-// import "./general.css";
 import getData from "../helpers/getData";
 
-import { createSignal, For } from "solid-js";
-import { createStore } from "solid-js/store";
-
-import Test from "../Test.jsx";
-
-// import {NormalCard} from "./card/CardTemplates.js"
+import { createSignal, For, onMount } from "solid-js";
+import urls from "../urls";
 
 let current = {};
 
 function List() {
-  const [state, setState] = createStore({
-    nodes: {
-      test: {
-        position: {
-          x: 500,
-          y: 250,
-        },
-        node_id: "test",
-      },
-    },
-  });
+  const [scripts, setScripts] = createSignal([]);
+
+  const fetchScripts = async () => {
+    let scripts = await getData(`${urls.fetch}/api/script/get_all`);
+    scripts = scripts.sort((a, b) => a > b);
+    console.log(scripts.length);
+    setScripts(scripts);
+  };
+
+  const removeScript = async (script_name) => {
+    let result = await getData(
+      `${urls.fetch}/api/script/delete/${script_name}`
+    );
+    console.log(result);
+  };
+
+  onMount(() => fetchScripts());
 
   return (
     <div className="App">
-      <header className="row">All Scripts</header>
-      <For each={Object.entries(state.nodes)}>
-        {([node_id, node]) => {
-          return (
-            <Test
-              position={node.position}
-              node_id={node_id}
-              nodes={state.nodes}
-              setState={setState}
-              test={state.nodes.test.position.x}
-            ></Test>
-          );
-        }}
-      </For>
+      <div style={{ overflow: "auto", height: "100vh" }}>
+        <For each={scripts()}>
+          {(script_name) => (
+            <div style={{ display: "flex", width: "100%" }}>
+              <a href={`/${script_name}`} target="_blank" style={{ flex: 1 }}>
+                {script_name}
+              </a>
+              {/* <button onClick={() => removeScript(script_name)}>delete </button> */}
+            </div>
+          )}
+        </For>
+      </div>
     </div>
   );
 }
