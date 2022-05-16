2ad30a941b90f0cfeada7d9ea43a7109c170e654
Vincent Van Dijck <vandijckv@gmail.com>
2022-05-16 17:13:35 +0200

    editor: getData/postData cleanup

--- editor/client_solid/src/helpers/getData.js
+++ editor/client_solid/src/helpers/getData.js
@@ -1,15 +1,24 @@
-async function getData(url = '') {
-  // Default options are marked with *
+async function getData(url = '', type = "json") {
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer'
+    });
 
-  const response = await fetch(url, {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  });
-  return response; // parses JSON response into native JavaScript objects
+    if (type === "json")
+      return await response.json();
+
+    return response;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+
+
 }
 
 export default getData;
--- editor/client_solid/src/helpers/postData.js
+++ editor/client_solid/src/helpers/postData.js
@@ -1,17 +1,19 @@
-async function postData(url = '', data = {}) {
+export default async function postData(url = '', data = {}, type = "json") {
   const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: { 'Content-Type': 'application/json' },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: type === 'json' ? JSON.stringify(data) : data
   });
-  return response; // parses JSON response into native JavaScript objects
-}
 
-export default postData;
+  console.log("RESPONSE,", response);
+
+  if (type === "json")
+    return await response.json();
+
+  return response;
+}
